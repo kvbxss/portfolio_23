ad37778ccf5520e502a8764f4416cd5305f9e177
kvbxss <kubadownar444@gmail.com>
2023-04-13 20:40:26 +0200

    index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-ReactDOM.createRoot(document.querySelector("#root")).render(<App />);
+root.render(
+<React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+</React.StrictMode>
+
+
+);
