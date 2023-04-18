b6f0bd1901496aa9ee62030c7463504c85e85807
kvbxss <kubadownar444@gmail.com>
2023-04-18 23:12:34 +0200

    fix

--- src/components/TopLine.jsx
+++ src/components/TopLine.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
 import lens from '../assets/lens.png';
-import { Link } from 'react-router-dom';
 
 const TopLine = () => {
   return (
