4f35e44db028e3963d65bc2b08a1c6cf6d32b139
kvbxss <kubadownar444@gmail.com>
2023-04-18 22:51:34 +0200

    imported all images

--- src/components/ExpertisePage.jsx
+++ src/components/ExpertisePage.jsx
@@ -2,6 +2,27 @@ import React from 'react'
 import styled from 'styled-components'
 import TopLine from './TopLine'
 import Sidebar from './Sidebar'
+import ImgFallback from './ImgFallback'
+
+import react from '../assets/expertise/react.webp'
+import reactF from '../assets/expertise/react.png'
+import js from '../assets/expertise/js.webp'
+import jsF from '../assets/expertise/js.png'
+import html from '../assets/expertise/html.webp'
+import htmlF from '../assets/expertise/html.png'
+import css from '../assets/expertise/css.webp'
+import cssF from '../assets/expertise/css.png'
+import style from '../assets/expertise/styled-components.webp'
+import styleF from '../assets/expertise/styled-components.png'
+import rest from '../assets/expertise/rest-api.webp'
+import restF from '../assets/expertise/rest-api.png'
+import gatsby from '../assets/expertise/gatsby.webp'
+import gatsbyF from '../assets/expertise/gatsby.png'
+import git from '../assets/expertise/git.webp'
+import gitF from '../assets/expertise/git.png'
+import figma from '../assets/expertise/figma.webp'
+import figmaF from '../assets/expertise/figma.png'
+
 
 const ExpertisePage = () => {
   return (
@@ -14,27 +35,43 @@ const ExpertisePage = () => {
             </Title>
             </TitleWrapper>
             <LineWrapper>
+                
                 <Element>
-                    <Line />
+                    <ImgWFallback src={react} fallback={reactF} alt='react'/>
+                </Element>
+                <ElementTwo>
+                    <ImgWFallback src={js} fallback={jsF} alt='js'/>
+                </ElementTwo>
+                    
+                <Element>
+                    <ImgWFallback src={html} fallback={htmlF} alt='html'/>
+                </Element>
+                    
+                <ElementTwo>
+                    <ImgWFallback src={css} fallback={cssF} alt='css'/>
+                </ElementTwo>
+                    
+                <Element>
+                    <ImgWFallback src={style} fallback={styleF} alt='styled-components' />
                 </Element>
 
-                <Element>
-                    <Line />
-                </Element>
+                <ElementTwo>
+                    <ImgWFallback src={rest} fallback={restF} alt='rest'/>
+                </ElementTwo>
 
                 <Element>
-                    <Line />
+                    <ImgWFallback src={gatsby} fallback={gatsbyF} alt='gatsby' />
                 </Element>
 
-                <Element>
-                    <Line />
-                </Element>
+                <ElementTwo>
+                    <ImgWFallback src={git} fallback={gitF} alt='git'/>
+                </ElementTwo>
 
                 <Element>
-                    <Line />
+                    <ImgWFallback src={figma} fallback={figmaF} alt='figma' />
                 </Element>
+
             </LineWrapper>
-        
         <SidebarWrapper>        
           <Sidebar />
           </SidebarWrapper>
@@ -48,15 +85,15 @@ export default ExpertisePage
 const Expertise = styled.div`
     width: 100vw;
     height: 100vh;
-    display: flex;
-    flex-direction: column;
 `
 
 const ExpertiseContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;`
+    justify-content: center;
+    margin-top: 50px;
+    `
 
 const TitleWrapper = styled.div`
     display: flex;
@@ -66,7 +103,8 @@ const TitleWrapper = styled.div`
 const Title = styled.h1`
     font-size: 144px;
     font-family: 'Playfair Display', serif;
-    font-weight: 400;`
+    font-weight: 400;
+    color: #E1A10B;`
 
 const LineWrapper = styled.div`
     display: flex;
@@ -74,26 +112,52 @@ const LineWrapper = styled.div`
     align-items: center;
     justify-content: center;
     padding: 12px;
-    gap: 8px;`
+    gap: 32px;
+    margin-top: 100px;
+    margin-right: 100px;`
 
 const Element = styled.div`
     display: flex;
-    flex-direction: column;
     align-items: center;
+    justify-content: center;
     width: 136px;
+    height: 136px;
+    background-color: #FFFFFF;
+    border-radius: 10%;
+    margin-bottom: 136px;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+        transform: translateY(-2px);
+        box-shadow: 0 10px 10px rgba(0,0,0,0.2);
+    }
     `
 
-const Line = styled.div`
-    position: absolute;
-    width: 832px;
-    height: 0px;
-    border: 1px solid #A1A1A1;
-    transform: rotate(90deg);`
+const ElementTwo = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 136px;
+    height: 136px;
+    background-color: #FFFFFF;
+    border-radius: 10%;
+    margin-top: 136px;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+        transform: translateY(-2px);
+        box-shadow: 0 10px 10px rgba(0,0,0,0.2);
+    }
+    
+    `
 
 const SidebarWrapper = styled.div`
     width: 100vw;
     display: flex;
-    flex-direction: column;
     justify-content: flex-end;
-    align-items: flex-end;`
+    align-items: flex-end;
+    margin-top: 150px;`
 
+const ImgWFallback = styled(ImgFallback)`
+    width: 120px;
+    `
