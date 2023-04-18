5e377972ff10ad1044edb10a9c271f840c6c7e09
kvbxss <kubadownar444@gmail.com>
2023-04-18 22:51:17 +0200

    idk

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
@@ -10,6 +11,7 @@ root.render(
         <App />
     </BrowserRouter>
 </React.StrictMode>
+
 
 
 );
