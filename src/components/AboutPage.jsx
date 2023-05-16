12191844e635d4b245846b5f342054925e88ee43
kvbxss <kubadownar444@gmail.com>
2023-05-17 00:25:04 +0200

    started about page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import TopLine from './TopLine'
 
 const AboutPage = () => {
@@ -6,11 +7,43 @@ const AboutPage = () => {
     <About id="about">
       <TopLine />
       <AboutWrapper>
-        
+        <ImageWrapper>
+          <Img/>
+        </ImageWrapper>
+        <TextWrapper>
+          <Text>
+          About Me
+          </Text>
+        </TextWrapper>
       </AboutWrapper>
     </About>
   )
 }
 
 export default AboutPage;
+
+const About = styled.div`
+width: 100vw;
+height: 100vh;
+`
+
+const AboutWrapper = styled.div``
+
+const ImageWrapper = styled.div``
+
+const Img = styled.img`
+    width: 100%;`
+
+const TextWrapper = styled.div`
+    display: flex;
+    padding-top: 100px;
+    justify-content: center;
+    align-items: center;
+`
+
+const Text = styled.h1`
+    font-family: 'Playfair Display', serif;
+    font-size: 144px;
+    font-weight: 400;
+`
 
