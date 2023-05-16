1d05dfed7fe65beec80739e87661e8d448961508
kvbxss <kubadownar444@gmail.com>
2023-05-17 00:24:46 +0200

    diff photo border

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { GlobalFonts } from '../GlobalFonts';
 import TopLine from './TopLine';
+import me from '../assets/me.png'
 
 const HomePage = () => {
   return (
@@ -18,14 +19,19 @@ const HomePage = () => {
         </NameWrapper>
         <DevWrapper>
           <Dev>
-            React Frontend Developer
+            react frontend developer
           </Dev>
         </DevWrapper>
+        <ImgTextWrap>
+        <ImageWrapper>
+          <Img src={me} alt='me'/>
+        </ImageWrapper>
         <TextWrapper>
         <Text>
           Use menu to navigate
         </Text>
         </TextWrapper>
+        </ImgTextWrap>
     </Home>
   )
 }
@@ -42,7 +48,8 @@ const NameWrapper = styled.div`
     align-items: center;
     justify-content: flex-start;
     margin-left: 112px;
-    margin-top: 64px;`
+    margin-top: 64px;
+    `
 
 const Surname = styled.h1`
     font-family: 'TanPearl';
@@ -61,7 +68,8 @@ const DevWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-end;
-    margin-right: 158px;`
+    margin-right: 158px;
+    `
 
 const Dev = styled.h2`
     font-family: 'TheSeasons';
@@ -70,12 +78,46 @@ const Dev = styled.h2`
     font-weight: 400;
     `
 
+
+const ImgTextWrap = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding-bottom: 50px;
+`
+
+const ImageWrapper = styled.div`
+    width: 300px;
+    border-radius: 50%;
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    margin-left: 80px;
+    padding-bottom: 50px;
+    filter: blur(5px) drop-shadow(50px 4px 4px rgba(0, 0, 0, 0.25));
+    transition: 0.5s;
+    &:hover {
+        filter: blur(0px) drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    }
+`
+
+const Img = styled.img`
+    width: 400px;
+    border-radius: 50%;
+    border: 2px solid #E1E1E1;
+    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    filter: grayscale(40%);
+`
+
 const TextWrapper = styled.div`
     display: flex;
     align-items: flex-end;
     justify-content: flex-end;
     margin-right: 158px;
-    margin-top: 474px`
+    padding-left: 50px;
+    margin-bottom: 80px;
+    `
 
 const Text = styled.h3`
     font-family: 'TheSeasons';
