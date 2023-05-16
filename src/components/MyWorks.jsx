48bd73a9f43a35dcf46388c15415ded171e2bbf0
kvbxss <kubadownar444@gmail.com>
2023-05-17 00:24:38 +0200

    none

---
+++ src/components/MyWorks.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const MyWorks = () => {
+  return (
+    <Works>
+      
+    </Works>
+  )
+}
+
+export default MyWorks;
+
+const Works = styled.div``
